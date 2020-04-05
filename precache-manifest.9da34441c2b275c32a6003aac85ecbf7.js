cc64543c5f38c36b92cd3965c815a1911dc1a8cd
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-04-05 16:22:01 +0200

    Updates

---
+++ precache-manifest.9da34441c2b275c32a6003aac85ecbf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a414ce46b2a7eca25b28e1288d5a0fe6",
+    "url": "/react_news_app_new/index.html"
+  },
+  {
+    "revision": "5109d3fa17d44f5b6a40",
+    "url": "/react_news_app_new/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ea9da2dbf2db056ad28e",
+    "url": "/react_news_app_new/static/css/main.811d96a3.chunk.css"
+  },
+  {
+    "revision": "5109d3fa17d44f5b6a40",
+    "url": "/react_news_app_new/static/js/2.67b61e62.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react_news_app_new/static/js/2.67b61e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9da2dbf2db056ad28e",
+    "url": "/react_news_app_new/static/js/main.0ab2a02d.chunk.js"
+  },
+  {
+    "revision": "1026354186006591855f",
+    "url": "/react_news_app_new/static/js/runtime-main.1d93ad57.js"
+  }
+]);
